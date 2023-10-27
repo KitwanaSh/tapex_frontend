3b7646ee81f7704313e199e46d60ac5410489e2c
KitwanaSh <rodriguekitweze@gmail.com>
2023-10-27 10:36:22 +0000

    Feat: Update the css

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -11,18 +11,20 @@ const items = [
 
 const ItemList = ({address}) => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-center">Student Bank Data</h1>
+    // <div className="container mx-auto flex flex-col justify-center align-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-2 text-center">Student Bank Data</h1>
       {/* <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 "> */}
       <div>
         <h4>Home <span>My profile</span></h4>
         <p>Welcome: {address}</p>
       </div>
-      <div className="flex flex-wrap justify-center flex-col items-center h-screen gap-4">
+      <div className="flex flex-wrap justify-center flex-col  h-screen gap-4">
         {items.map(item => (
           <div
             key={item.id}
-            className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300 ease-in-out"
+            className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transform
+            hover:scale-105 transition-transform duration-300 ease-in-out "
           >
             <p className="text-xl font-semibold">{item.text}</p>
           </div>
